bb066e8468423ec7db3d5844c78cc7da16ee0a77
Anmol Sharma <53692108+AnMol12499@users.noreply.github.com>
2023-12-15 21:23:11 -0600

    Update download_datasets.sh

--- download_datasets.sh
+++ download_datasets.sh
@@ -16,21 +16,4 @@ gdown "10xtuwzWJn_7SfOi7xnXjXTF9B3oz6zdO&confirm=t"
 
 cd ..
 unzip dataset_files/spider.zip -d dataset_files/ori_dataset/
-unzip dataset_files/spider-dk.zip -d dataset_files/ori_dataset/
-mv dataset_files/ori_dataset/spider-dk dataset_files/ori_dataset/spider_dk
-unzip dataset_files/spider-syn.zip -d dataset_files/ori_dataset/
-mv dataset_files/ori_dataset/spider-syn dataset_files/ori_dataset/spider_syn
-unzip dataset_files/spider-realistic.zip -d dataset_files/ori_dataset/
-mv dataset_files/ori_dataset/spider-realistic dataset_files/ori_dataset/spider_realistic
-rm -r dataset_files/ori_dataset/spider_dates
-unzip dataset_files/spider-dates.zip -d dataset_files/ori_dataset/
-mv dataset_files/ori_dataset/spider-dates dataset_files/ori_dataset/spider_dates
-rm -r dataset_files/ori_dataset/spider_desc
-unzip dataset_files/spider-desc.zip -d dataset_files/ori_dataset/
-mv dataset_files/ori_dataset/spider-desc dataset_files/ori_dataset/spider_desc
-rm -r dataset_files/ori_dataset/dk_spider_dates
-unzip dataset_files/dk-spider-dates.zip -d dataset_files/ori_dataset/
-mv dataset_files/ori_dataset/dk-spider-dates dataset_files/ori_dataset/dk_spider_dates
-cp dataset_files/ori_dataset/spider/tables.json dataset_files/ori_dataset/spider_syn
-cp dataset_files/ori_dataset/spider/dev_gold.sql dataset_files/ori_dataset/spider_syn
 chmod -R 777 dataset_files/
