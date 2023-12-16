457ab338d60e23f2d6191e30e7bc9054f27d2e0b
Anmol Sharma <53692108+AnMol12499@users.noreply.github.com>
2023-12-15 22:01:54 -0600

    Update outputeval.py

--- models/picard_runner/outputeval.py
+++ models/picard_runner/outputeval.py
@@ -1,26 +1,24 @@
 import json
+import sys
 
 def process_data(json_file_path):
-  
     with open(json_file_path, 'r') as file:
         data = json.load(file)
 
-    
     with open('gold.txt', 'w') as gold_file, open('pred.txt', 'w') as pred_file:
         for item in data:
-            # Get the actual SQL query
             gold = item['query']
-
-          
             prediction_full = item['prediction']
             pred = prediction_full.split('|')[1].strip() if '|' in prediction_full else prediction_full.strip()
 
-            # Write to files
             gold_file.write(gold + '\t' + item['db_id'] + '\n')
             pred_file.write(pred + '\n')
 
-
 if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python output.py <json_file_path>")
+        sys.exit(1)
 
-   
-process_data(json_file_path)
+    json_file_path = sys.argv[1]
+    process_data(json_file_path)
+
